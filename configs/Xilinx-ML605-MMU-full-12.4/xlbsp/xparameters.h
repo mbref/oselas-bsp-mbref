956cb1410a6887ae5def0d03893391e12aa421bd
Stephan Linz <linz@li-pro.net>
2011-01-16 15:37:16 +0100

    Xilinx ML605: new configuration for MMU-full-12.4
      * platform: show device tree under /proc/device-tree
      * xlbsp: new simple register bank (plbv46_mbref_reg)
    
    Signed-off-by: Stephan Linz <linz@li-pro.net>

--- configs/Xilinx-ML605-MMU-full-12.4/xlbsp/xparameters.h
+++ configs/Xilinx-ML605-MMU-full-12.4/xlbsp/xparameters.h
@@ -44,7 +44,8 @@
 
 /* Interrupt controller is xps_intc_0 */
 #define XILINX_INTC_BASEADDR                     0x8AFF0000
-#define XILINX_INTC_NUM_INTR_INPUTS              9
+#define XILINX_INTC_NUM_INTR_INPUTS              10
+/* mbref_reg_0_IP2INTC_Irpt */
 /* xps_iic_0_IIC2INTC_Irpt */
 /* PCIe_Bridge_IP2INTC_Irpt */
 /* xps_sysace_0_SysACE_IRQ */
