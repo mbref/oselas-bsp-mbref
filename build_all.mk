ba892f39f55aa7efc8dac15e211748344dd21e7d
Stephan Linz <linz@li-pro.net>
2012-06-26 22:49:59 +0200

    common: improve build_all.mk make script
        - automatic endianess detection
        - extract and print out toolchain name and endianess
        - extract and print out ptx-platform name and version
        - generalize build target over all kind of endianess
    
    Signed-off-by: Stephan Linz <linz@li-pro.net>

--- build_all.mk
+++ build_all.mk
@@ -25,16 +25,16 @@ endif
 
 ifeq ($(PETALOGIX),true)
 # use the Petalogix Microblaze toolchain preperated for ptxdist
-TOOLCHAIN	:= /opt/tools-4.1.2-mb-petalogix-v2.1/linux-i386/microblaze-unknown-linux-gnu/bin
+TOOLCHAIN_BE	:= /opt/tools-4.1.2-mb-petalogix-v2.1/linux-i386/microblaze-unknown-linux-gnu/bin
 TOOLCHAIN_LE	:= /opt/tools-4.1.2-mb-petalogix-v2.1/linux-i386/microblazeel-unknown-linux-gnu/bin
 else
 ifeq ($(XILINX),true)
 # use the Xilinx Microblaze toolchain preperated for ptxdist
-TOOLCHAIN	:= /opt/tools-4.1.2-mb-xilinx-v2.0/microblaze-unknown-linux-gnu/bin
+TOOLCHAIN_BE	:= /opt/tools-4.1.2-mb-xilinx-v2.0/microblaze-unknown-linux-gnu/bin
 TOOLCHAIN_LE	:= /opt/tools-4.1.2-mb-xilinx-v2.0/microblazeel-unknown-linux-gnu/bin
 else
 # use the Crosstool-NG Microblaze toolchain preperated for ptxdist
-TOOLCHAIN	:= /opt/tools-4.1.2-mb-ctng-v1.4/microblaze-unknown-linux-gnu/bin
+TOOLCHAIN_BE	:= /opt/tools-4.1.2-mb-ctng-v1.4/microblaze-unknown-linux-gnu/bin
 TOOLCHAIN_LE	:= /opt/tools-4.1.2-mb-ctng-v1.4/microblazeel-unknown-linux-gnu/bin
 endif
 endif
@@ -49,6 +49,16 @@ define gen_2platformfile
 $(eval 2PLATFORMFILE_$(subst _,-,$(notdir $(patsubst %/,%,$(dir $(1))))) := $(1))
 endef
 
+define gen_2platformname
+$(eval 2PLATFORMNAME_$(subst _,-,$(notdir $(patsubst %/,%,$(dir $(1))))) := \
+	$(shell PTX_AUTOBUILD_DESTDIR='' source "$(1)" && echo "$${PTXCONF_PLATFORM}"))
+endef
+
+define gen_2platformvers
+$(eval 2PLATFORMVERS_$(subst _,-,$(notdir $(patsubst %/,%,$(dir $(1))))) := \
+	$(shell PTX_AUTOBUILD_DESTDIR='' source "$(1)" && echo "$${PTXCONF_PLATFORM_VERSION}"))
+endef
+
 define gen_2platformdir
 $(eval 2PLATFORMDIR_$(subst _,-,$(notdir $(patsubst %/,%,$(dir $(1))))) := \
 	$(shell PTX_AUTOBUILD_DESTDIR='' source "$(1)" && echo "platform-$${PTXCONF_PLATFORM}"))
@@ -59,9 +69,37 @@ $(eval 2IMAGEDIR_$(subst _,-,$(notdir $(patsubst %/,%,$(dir $(1))))) := \
 	$(shell PTX_AUTOBUILD_DESTDIR='' source "$(1)" && echo "platform-$${PTXCONF_PLATFORM}/images"))
 endef
 
+define gen_2toolchain
+$(eval 2TOOLCHAIN_$(subst _,-,$(notdir $(patsubst %/,%,$(dir $(1))))) := \
+	$(if $(shell PTX_AUTOBUILD_DESTDIR='' source "$(1)" && echo "$${PTXCONF_ENDIAN_LITTLE}"), \
+		$(TOOLCHAIN_LE), \
+	$(if $(shell PTX_AUTOBUILD_DESTDIR='' source "$(1)" && echo "$${PTXCONF_ENDIAN_BIG}"), \
+		$(TOOLCHAIN_BE), \
+	$(error "$(1): can not determine endianess (big or little)")))) \
+$(eval $(subst _,-,$(notdir $(patsubst %/,%,$(dir $(1))))).toolchain: \
+	$(if $(shell PTX_AUTOBUILD_DESTDIR='' source "$(1)" && echo "$${PTXCONF_ENDIAN_LITTLE}"), \
+		$(TOOLCHAIN_LE)/ptxconfig, \
+	$(if $(shell PTX_AUTOBUILD_DESTDIR='' source "$(1)" && echo "$${PTXCONF_ENDIAN_BIG}"), \
+		$(TOOLCHAIN_BE)/ptxconfig, \
+	$(error "$(1): can not determine endianess (big or little)"))))
+endef
+
+define gen_2toolchainname
+$(eval 2TOOLCHAINNAME_$(subst _,-,$(notdir $(patsubst %/,%,$(dir $(1))))) := \
+	$(if $(shell PTX_AUTOBUILD_DESTDIR='' source "$(1)" && echo "$${PTXCONF_ENDIAN_LITTLE}"), \
+		$(shell awk -F'[=]' '{gsub(/\"/,""); printf("%s -- little endian", $$(NF))}' $(TOOLCHAIN_LE)/ptxconfig), \
+	$(if $(shell PTX_AUTOBUILD_DESTDIR='' source "$(1)" && echo "$${PTXCONF_ENDIAN_BIG}"), \
+		$(shell awk -F'[=]' '{gsub(/\"/,""); printf("%s -- big endian", $$(NF))}' $(TOOLCHAIN_BE)/ptxconfig), \
+	$(error "$(1): can not determine endianess (big or little)"))))
+endef
+
 $(foreach pltfile,$(PLATFORMFILES),$(call gen_2platformfile,$(pltfile)))
+$(foreach pltfile,$(PLATFORMFILES),$(call gen_2platformname,$(pltfile)))
+$(foreach pltfile,$(PLATFORMFILES),$(call gen_2platformvers,$(pltfile)))
 $(foreach pltfile,$(PLATFORMFILES),$(call gen_2platformdir,$(pltfile)))
 $(foreach pltfile,$(PLATFORMFILES),$(call gen_2imgdir,$(pltfile)))
+$(foreach pltfile,$(PLATFORMFILES),$(call gen_2toolchain,$(pltfile)))
+$(foreach pltfile,$(PLATFORMFILES),$(call gen_2toolchainname,$(pltfile)))
 
 STATEDIR	:= gstate
 DISTDIR		:= dist
@@ -80,24 +118,17 @@ $(TBZ2_PREFIX)%$(TBZ2_SUFFIX): $(STATEDIR)/%.build | mkdirs
 	@echo 'tar -C "$(2IMAGEDIR_$(*))" -cvjf "$(@)" "."' | fakeroot
 	@rm -rf "$(2PLATFORMDIR_$(*))"
 
-# special AXI little endian systems
-$(STATEDIR)/Avnet-S6LX150T-AXI-%.build: $(TOOLCHAIN_LE)/ptxconfig | mkdirs
-	@echo "found AXI little endian system"
-	@echo "building Avnet-S6LX150T-AXI-${*} with" $(shell awk -F'[=]' '{printf("%s", $$(NF))}' $<)
-	$(NICE) $(PTXDIST) images --ptxconfig=$(CONFIGFILE) \
-	  --toolchain=$(TOOLCHAIN_LE) --platformconfig=$(2PLATFORMFILE_Avnet-S6LX150T-AXI-$(*))
-
-$(STATEDIR)/Xilinx-ML605-AXI-%.build: $(TOOLCHAIN_LE)/ptxconfig | mkdirs
-	@echo "found AXI little endian system"
-	@echo "building Xilinx-ML605-AXI-${*} with" $(shell awk -F'[=]' '{printf("%s", $$(NF))}' $<)
-	$(NICE) $(PTXDIST) images --ptxconfig=$(CONFIGFILE) \
-	  --toolchain=$(TOOLCHAIN_LE) --platformconfig=$(2PLATFORMFILE_Xilinx-ML605-AXI-$(*))
-
 # generic big endian systems
-$(STATEDIR)/%.build: $(TOOLCHAIN)/ptxconfig | mkdirs
-	@echo "building ${*} with" $(shell awk -F'[=]' '{printf("%s", $$(NF))}' $<)
+$(STATEDIR)/%.build: %.toolchain | mkdirs
+	@echo "******************************************************************************"
+	@echo "building:  $(2PLATFORMNAME_$(*))"
+	@echo " version:  $(2PLATFORMVERS_$(*))"
+	@echo "    with:  $(2TOOLCHAINNAME_$(*))"
+	@echo "******************************************************************************"
+	@echo
 	$(NICE) $(PTXDIST) images --ptxconfig=$(CONFIGFILE) \
-	  --toolchain=$(TOOLCHAIN) --platformconfig=$(2PLATFORMFILE_$(*))
+	  --toolchain=$(2TOOLCHAIN_$(*)) \
+	  --platformconfig=$(2PLATFORMFILE_$(*))
 
 mkdirs:
 	@mkdir -p $(STATEDIR) $(DISTDIR)
@@ -105,6 +136,6 @@ mkdirs:
 print-%:
 	@echo "$* is \"$($(*))\""
 
-help: print-VERSION print-BUILDDATE print-PTXDIST print-TOOLCHAIN print-TOOLCHAIN_LE
+help: print-VERSION print-BUILDDATE print-PTXDIST print-TOOLCHAIN_BE print-TOOLCHAIN_LE
 	@echo "Available tarball targets:"
 	@for i in $(TBZ2S); do echo $$i; done;
